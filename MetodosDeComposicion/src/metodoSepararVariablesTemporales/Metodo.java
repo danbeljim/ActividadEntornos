cb7a45a58c152dd271f2086bcef89bbf6153b5a9
andres <you@example.com>
2020-03-27 19:31:01 +0100

    Separar variables temporales

---
+++ MetodosDeComposicion/src/metodoSepararVariablesTemporales/Metodo.java
@@ -0,0 +1,12 @@
+package metodoSepararVariablesTemporales;
+
+public class Metodo {
+
+    void variablesTemporales(double pi, double r){
+        double temp = 2 * pi * r;
+        System.out.println(temp);
+        temp = pi * r * r;
+        System.out.println(temp);
+    }
+
+}
